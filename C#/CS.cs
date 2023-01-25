548cd40931b461bcbab46ba8879df7c387415b18
Shin <86329731+Shin-Ideal@users.noreply.github.com>
2023-01-25 11:44:08 +0900

    First Commit

---
+++ C#/CS.cs
@@ -0,0 +1,9 @@
+using System;
+
+class CS{
+
+    public static void Main(){
+        Console.WriteLine("Hello Wolrd!");
+    }
+
+}
---
+++ Java/Java.java
@@ -0,0 +1,5 @@
+class Java{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
